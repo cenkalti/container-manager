aefa189740b584095a004d7607ee5e64ed687ceb
Cenk Alti <cenkalti@gmail.com>
2020-03-09 16:07:05 +0300

    better config

--- config.go
+++ config.go
@@ -25,11 +25,10 @@ type Container struct {
 	Entrypoint  []string
 	Cmd         []string
 	StopSignal  string
-	StopTimeout *int
+	StopTimeout time.Duration
 	NetworkMode string
-	Env         []string
+	Env         map[string]string
 	Binds       []string
-	DNS         []string
 }
 
 func (c *Config) setDefaults() {
@@ -67,17 +66,25 @@ func getContainerDefinion(name string) *Container {
 }
 
 func (c *Container) containerConfig(name string) *container.Config {
+	env := make([]string, 0, len(c.Env))
+	for k, v := range c.Env {
+		env = append(env, k+"="+v)
+	}
+	var stopTimeout *int
+	if sec := int(c.StopTimeout / time.Second); sec > 0 {
+		stopTimeout = &sec
+	}
 	return &container.Config{
 		Hostname:     name,
-		AttachStdout: true,          // Attach the standard output
-		AttachStderr: true,          // Attach the standard error
-		Env:          c.Env,         // List of environment variable to set in the container
-		Cmd:          c.Cmd,         // Command to run when starting the container
-		Image:        c.Image,       // Name of the image as it was passed by the operator (e.g. could be symbolic)
-		WorkingDir:   c.WorkingDir,  // Current directory (PWD) in the command will be launched
-		Entrypoint:   c.Entrypoint,  // Entrypoint to run when starting the container
-		StopSignal:   c.StopSignal,  // Signal to stop a container
-		StopTimeout:  c.StopTimeout, // Timeout (in seconds) to stop a container
+		AttachStdout: true,         // Attach the standard output
+		AttachStderr: true,         // Attach the standard error
+		Env:          env,          // List of environment variable to set in the container
+		Cmd:          c.Cmd,        // Command to run when starting the container
+		Image:        c.Image,      // Name of the image as it was passed by the operator (e.g. could be symbolic)
+		WorkingDir:   c.WorkingDir, // Current directory (PWD) in the command will be launched
+		Entrypoint:   c.Entrypoint, // Entrypoint to run when starting the container
+		StopSignal:   c.StopSignal, // Signal to stop a container
+		StopTimeout:  stopTimeout,  // Timeout (in seconds) to stop a container
 		Labels: map[string]string{
 			containerVersionKey: c.Version,
 		}, // List of labels set to this container
@@ -88,7 +95,6 @@ func (c *Container) hostConfig() *container.HostConfig {
 	return &container.HostConfig{
 		Binds:         c.Binds,                              // List of volume bindings for this container
 		NetworkMode:   container.NetworkMode(c.NetworkMode), // Network mode to use for the container
-		DNS:           c.DNS,                                // List of DNS server to lookup
 		RestartPolicy: container.RestartPolicy{Name: "always"},
 	}
 }
